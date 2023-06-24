281c3617be0b4c206df52de59d3e9f5492a172ce
Gabriel Daher <gabriel.daher.p@mail.pucv.cl>
2023-06-24 17:19:26 -0400

    Implementacion Mensaje de Error en Formulario V1

--- front-end/src/app/home/registro/registro.page.ts
+++ front-end/src/app/home/registro/registro.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage {
-  constructor(private router: Router) {}
+  formularioRegistroPersonal: FormGroup;
+  mostrar: boolean = false;
+  errorMessage: { [key: string]: string };
+
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.formularioRegistroPersonal = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      rut: ['', Validators.required],
+      sexo: [''],
+      paisResidencia: ['']
+    });
+    this.errorMessage = {};
+  }
 
   goToHome() {
     this.router.navigate(['/home']);
+  }
+
+  continuarPrueba() {
+    this.mostrarFormularioFoto();
+  }
+
+  mostrarFormularioFoto() {
+    this.mostrar = !this.mostrar;
+  }
+
+  enviarRegistro() {
+    if (this.formularioRegistroPersonal.invalid) {
+      const controls = this.formularioRegistroPersonal.controls;
+      for (const c in controls) {
+        if (controls[c].invalid) {
+          if (controls[c].errors?.['pattern']) {
+            console.log('Invalido: ' + this.errorMessage[c + 'Invalido']);
+            // Realiza otras acciones si es necesario
+            return;
+          }
+          console.log(c);
+          console.log(this.errorMessage[c]);
+          // Realiza otras acciones si es necesario
+          return;
+        }
+      }
+    }
   }
 }
