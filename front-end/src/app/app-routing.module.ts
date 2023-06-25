1dbe340f88e49692de15cefe1d9e1ff911c0ec4d
Gabriel Daher <gabriel.daher.p@mail.pucv.cl>
2023-06-24 22:36:23 -0400

    Implementacion de DatePicker para el genero e implementacion de listado de paises

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -8,24 +8,14 @@ const routes: Routes = [
   },
   {
     path: 'home/registro', // Ruta ajustada para incluir la carpeta "registro" dentro de "home"
-    loadChildren: () => import('./home/registro/registro.module').then(m => m.RegistroPageModule)
+    loadChildren: () => import('../app/home/registro/registro.module').then(m => m.RegistroPageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-<<<<<<< HEAD
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
 
-=======
-  },
-
-
->>>>>>> 7d696ae14f1b88dd6e1e84ad5c5a5f123e1d0e5d
 ];
 
 @NgModule({
