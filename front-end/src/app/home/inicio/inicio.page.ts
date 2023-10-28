c958b38b0f540bea88b140429d68902efbe1eb25
DiegoVegaMaureira <89675179+DiegoVegaMaureira@users.noreply.github.com>
2023-10-28 12:52:04 -0300

    FUNCIONA LA BASE DE DATOOOOOOOOOS TYU MAMAMAMAMAMAMMAMAMAMAMA

--- front-end/src/app/home/inicio/inicio.page.ts
+++ front-end/src/app/home/inicio/inicio.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, IonCard } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { MenuController } from '@ionic/angular';
@@ -9,6 +9,7 @@ import { getDocs } from '@firebase/firestore/lite';
 import { FirebaseFirestore } from '@firebase/firestore-types';
 import { ProductoPage } from './producto/producto.page';
 import { DocumentData } from '@angular/fire/compat/firestore';
+import { NgFor } from '@angular/common';
 
 interface PageItem {
   title: string;
@@ -37,6 +38,7 @@ export class InicioPage implements OnInit {
   ];
 
   db = getFirestore();
+  productosFB: any[] = [];
 
   constructor(
     private router: Router,
@@ -48,13 +50,46 @@ export class InicioPage implements OnInit {
 
   ngOnInit() {
     this.generateItems();
-
+    this.llenarInicio();
   }
+
+  async cargarProductos(){
+    const querySnapshot = await getDocs(collection(this.db, 'tu-coleccion'));
+    querySnapshot.forEach((doc) => {
+      this.productosFB.push(doc.data());
+    });
+  }
+
+  
 
   private generateItems() {
     const count = this.items.length + 1;
     for (let i = 0; i < 50; i++) {
       this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  llenarInicio() {
+    //declaracion de varuables
+    let ion_card = document.createElement('ion-card');
+    let img = document.createElement('img');
+    let ion_card_header = document.createElement('ion-card-header');
+    let ion_card_title = document.createElement('ion-card-title');
+    let ion_card_subtitle = document.createElement('ion-card-subtitle');
+    let ion_card_content = document.createElement('ion-card-content');
+    let paneles = document.getElementById('paneles');
+
+    //iteracion con los datos por producto
+    for (var producto of this.productosFB) {
+      img.setAttribute('src', 'https://i1.sndcdn.com/artworks-F11dor2dKfIo0DA6-zRbrxg-t500x500.jpg');
+      ion_card_title.innerText = producto.nombre;
+      ion_card_subtitle.innerText = '$' + producto.precio;
+      ion_card_content.innerText = producto.descripcion.substring(0,20);
+      ion_card_header.append(ion_card_title);
+      ion_card.append(ion_card_header);
+      ion_card.append(ion_card_content);
+      paneles?.append(ion_card);
+      console.log(producto.nombre);
     }
   }
   
